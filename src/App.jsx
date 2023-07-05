d28378d8e16a98409453a1dfc39ab8f5c9e827ca
Jakub Lis <jakublis@vp.pl>
2023-07-05 13:40:10 +0200

    add csv support

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,45 @@ function App() {
 	function handleUpload(e) {
 		const reader = new FileReader();
 		reader.readAsBinaryString(e.target.files[0]);
-		reader.onload = (e) => {
-			const data = e.target.result;
-			const workbook = XLSX.read(data, { type: "binary" });
-			const sheetName = workbook.SheetNames[0];
-			const sheet = workbook.Sheets[sheetName];
-			const parsedData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+		reader.onload = (ev) => {
+			const data = ev.target.result;
+			let parsedData;
+			let workbook;
+			let sheetName;
+			let sheet;
+			console.log(e.target.files[0].type);
+			switch (e.target.files[0].type) {
+				case "application/vnd.ms-excel":
+					workbook = XLSX.read(data, { type: "binary" });
+					sheetName = workbook.SheetNames[0];
+					sheet = workbook.Sheets[sheetName];
+					parsedData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+					break;
+				case "text/csv":
+					parsedData = data
+						.slice(data.indexOf("\n") + 1)
+						.split("\n")
+						.map((element) => element.split(","));
+
+					parsedData = parsedData.map((element) => {
+						return element.map((el) => {
+							return el.trim();
+						});
+					});
+					break;
+			}
 			console.log(parsedData);
 		};
 	}
 	return (
 		<>
-			<input id="upload" type="file" name="files[]" onChange={handleUpload} />
+			<input
+				id="upload"
+				type="file"
+				name="files[]"
+				onChange={handleUpload}
+				accept=".csv, .xls, .xlsx"
+			/>
 		</>
 	);
 }
